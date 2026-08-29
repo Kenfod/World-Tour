f03b7ad8b597bf91d3f30910457139d4ffd42ca7
Kenfod <kelvinnyagudi@yahoo.com>
2026-08-29 17:11:44 +0300

    Add dynamic routing configuration for individual city detail views: - Register parameterized path 'cities/:id' inside the AppLayout route branch - Create City view layout component utilizing react-router-dom useParams hook - Wrap CityItem template rows inside a Link component to forward parameters dynamically - Construct detail panel template layout with Wikipedia link targets and note elements

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,3 +1,4 @@
+import { useParams } from "react-router-dom";
 import styles from "./City.module.css";
 
 const formatDate = (date) =>
@@ -9,6 +10,9 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 function City() {
+  const { id } = useParams();
+  console.log(id);
+
   // TEMP DATA
   const currentCity = {
     cityName: "Lisbon",
@@ -18,6 +22,8 @@ function City() {
   };
 
   const { cityName, emoji, date, notes } = currentCity;
+
+  // return <h1>City {id}</h1>;
 
   return (
     <div className={styles.city}>
@@ -51,9 +57,7 @@ function City() {
         </a>
       </div>
 
-      <div>
-        <ButtonBack />
-      </div>
+      <div></div>
     </div>
   );
 }
